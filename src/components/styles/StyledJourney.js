1f45d7fd2cd991b528d97383153a68cc43116fe5
najah said <najaah8800@gmail.com>
2021-02-14 20:04:00 +0300

    initial commit with css done

---
+++ src/components/styles/StyledJourney.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledJourney = styled.div`
+  background: #565656;
+  font-size: 0.8rem;
+  padding: 2px 5px;
+  height: 20px;
+  display: flex;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 5px;
+`;
+export default StyledJourney;
